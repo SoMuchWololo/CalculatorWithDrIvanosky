2dd00b95820fc6fb4d61a8e96d63ad458d713578
Juan Ignacio Machado <60849397+SoMuchWololo@users.noreply.github.com>
2022-04-21 23:10:50 -0300

    Create Button-listener.js
    testeo de que los botones funcionan con console.log

---
+++ Button-listener.js
@@ -0,0 +1,64 @@
+/* aca arme un sensor de los botones, cada uno genera un console.log 
+    ya estan testeados, faltaria armar la visualizacion en el display y armar
+    las funciones correspondientes.
+*/
+document.getElementById("about").addEventListener("click", function() {
+    console.log("this is a about!");
+});
+document.getElementById("delete").addEventListener("click", function() {
+    console.log("this is a delete!");
+});
+document.getElementById("clear").addEventListener("click", function() {
+    console.log("this is a clear!");
+});
+document.getElementById("division").addEventListener("click", function() {
+    console.log("this is a division!");
+});
+document.getElementById("seven").addEventListener("click", function() {
+    console.log("this is a seven!");
+});
+document.getElementById("eight").addEventListener("click", function() {
+    console.log("this is a eight!");
+});
+document.getElementById("nine").addEventListener("click", function() {
+    console.log("this is a nine!");
+});
+document.getElementById("multiplication").addEventListener("click", function() {
+    console.log("this is a multiplication!");
+});
+document.getElementById("four").addEventListener("click", function() {
+    console.log("this is a four!");
+});
+document.getElementById("five").addEventListener("click", function() {
+    console.log("this is a five!");
+});
+document.getElementById("six").addEventListener("click", function() {
+    console.log("this is a six!");
+});
+document.getElementById("substraction").addEventListener("click", function() {
+    console.log("this is a substraction!");
+});
+document.getElementById("one").addEventListener("click", function() {
+    console.log("this is a one!");
+});
+document.getElementById("two").addEventListener("click", function() {
+    console.log("this is a two!");
+});
+document.getElementById("three").addEventListener("click", function() {
+    console.log("this is a three!");
+});
+document.getElementById("addition").addEventListener("click", function() {
+    console.log("this is a addition!");
+});
+document.getElementById("change-sign").addEventListener("click", function() {
+    console.log("this is a change-sign!");
+});
+document.getElementById("zero").addEventListener("click", function() {
+    console.log("this is a zero!");
+});
+document.getElementById("comma").addEventListener("click", function() {
+    console.log("this is a comma!");
+});
+document.getElementById("equal").addEventListener("click", function() {
+    console.log("this is a equal!");
+});
