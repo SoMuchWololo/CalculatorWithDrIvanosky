5cede1776d1b30d88a19ac8294d09d45576f8f1d
Juan Ignacio Machado <60849397+SoMuchWololo@users.noreply.github.com>
2022-04-25 19:57:24 -0300

    Update Listener-Keyboard.js

--- Listener-Keyboard.js
+++ Listener-Keyboard.js
@@ -1,12 +1,57 @@
-/* aca voy a armar un sensor para que tome los botones del teclado en vez
-tener que hacer click */
+/* aca faltarian el +, el ? y habria que ver
+que tecla definimos para solo cambiar el signo de - a + y viceversa*/
 
 document.addEventListener('keydown', function(event) {
     switch (event.keyCode) {
-        case 37:
-            /* 37 es el numero de la tecla a la izquierda,
-            habria que averiguar los numeros para las teclas que necesitamos*/
-            alert('Left was pressed');
+        
+        case 8:
+            alert('delete')
+            break;
+        case 46:
+            alert('clear')
+            break;
+        case 48:
+            alert('0');
+            break;
+        case 49:
+            alert('1');
+            break;
+        case 50:
+            alert('2');
+            break; 
+        case 51:
+            alert('3');
+            break; 
+        case 52:
+            alert('4');
+            break; 
+        case 53:
+            alert('5');
+            break; 
+        case 54:
+            alert('6');
+            break; 
+        case 55:
+            alert('7');
+            break; 
+        case 56:
+            alert('8');
+            break;
+        case 57:
+            alert('9');
+            break;
+        case 61:
+            alert('=');
+            break; 
+        case 173:
+            alert('-');
+            break; 
+        case 188:
+            alert(',');
+            break; 
+        case 191:
+            alert('/');     
+            break;  
         default:
             break;
     }
