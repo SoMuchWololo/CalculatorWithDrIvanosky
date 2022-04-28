89137cbe2b9db72966fe2c3040bb7ca853bd4649
Juan Ignacio Machado <60849397+SoMuchWololo@users.noreply.github.com>
2022-04-28 13:04:54 -0300

    Update Ejecutar.js

--- Ejecutar.js
+++ Ejecutar.js
@@ -4,10 +4,16 @@
 
 // ejecutar("1+2+3"); 
 
+/* el display deberia estar limitado a 9 caracteres para
+evitar el overflow de los caracteres. habria que armar una
+funcion que analize el largo del string y se ejecute si el 
+string es mas largo que 9 y genere un numero por ejemplo 8,3432*10^4
+Dicho string tampoco puede superar los 9 caracteres*/
+
 function buttonValue(){
     pantalla.innerHTML = buttonPress
 }
 pantalla = document.getElementById("display")
 console.log(pantalla)
-pantalla.innerHTML = "hola"
+pantalla.innerHTML = "1234,56789" 
 
